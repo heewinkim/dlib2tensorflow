a6ff34648c5e2c7a33fefff74c87311c7ee6b69d
heewinkim <heewin.kim@gmail.com>
2020-01-13 16:46:43 +0900

    main modify

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import os
 import sys
 import argparse
-
+import tensorflow as tf
 from converter.model import build_dlib_model
 from converter.weights import load_weights
-from converter.tensorflow import convert_to_tf_saved_model
-from converter.tensorflow import convert_to_tf_frozen_model
 
 def main(args):
     """ Main entry point """
@@ -15,16 +13,13 @@ def main(args):
     keras_model.summary()   
 
     # parse xml and load weights
-    load_weights(keras_model, args.xml_weights)
+    load_weights(keras_model, args.xml_path)
 
     # save it as h5
     keras_model.save("dlib_face_recognition_resnet_model_v1.h5")
 
     # save it as saved_model
-    convert_to_tf_saved_model(keras_model, os.curdir)
-
-    # save it as a frozen graph
-    convert_to_tf_frozen_model(keras_model, os.curdir)
+	tf.saved_model.save(keras_model,'./saved_model/')
 
 
 def parse_arg(argv):
@@ -32,7 +27,7 @@ def parse_arg(argv):
     arg_paser = argparse.ArgumentParser()
 
     arg_paser.add_argument(
-        '--xml-weights',
+        '--xml-path',
         type=str,
         required=True,
         help='Path to the dlib recognition xml file')    
